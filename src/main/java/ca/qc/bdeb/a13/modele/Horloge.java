93f0c0921e57624d42158ca89752863d3f6df5d7
Hosni Ghribi <ghribi.hosni7@gmail.com>
2025-11-19 10:40:22 -0500

    implement obserable interface in Horloge and add method to remove all observateurs

--- src/main/java/ca/qc/bdeb/a13/modele/Horloge.java
+++ src/main/java/ca/qc/bdeb/a13/modele/Horloge.java
@@ -1,22 +1,62 @@
 package ca.qc.bdeb.a13.modele;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
-public class Horloge {
+public class Horloge implements Observable, Runnable {
 
-    Calendar cal;
-
+    private Calendar cal;
     private String hour = "";
 
+    // Liste des observateurs inscrits
+    private List<Observateur> observateurs = new ArrayList<>();
+
+    public Horloge() {
+        // Initialisation du calendrier
+        this.cal = Calendar.getInstance();
+    }
+
+    @Override
+    public void addObservateur(Observateur obs) {
+        observateurs.add(obs);
+    }
+
+    @Override
+    public void updateObservateur() {
+        for (Observateur obs : observateurs) {
+            obs.update(hour);
+        }
+    }
+
+    @Override
+    public void delObservateur(Observateur obs) {
+        observateurs.remove(obs);
+    }
+
+    @Override
+    public void delAllObservateurs() {
+        observateurs.clear();
+    }
+
+    @Override
     public void run() {
         while (true) {
+            // Met à jour la date/heure courante
+            this.cal = Calendar.getInstance();
 
-            this.hour
-                    = this.cal.get(Calendar.HOUR_OF_DAY) + " : "
-                    + (this.cal.get(Calendar.MINUTE) < 10
-                    ? "0" + this.cal.get(Calendar.MINUTE) : this.cal.get(Calendar.MINUTE)) + ":"
-                    + ((this.cal.get(Calendar.SECOND) < 10)
-                    ? "0" + this.cal.get(Calendar.SECOND) : this.cal.get(Calendar.SECOND));
+            this.hour =
+                    this.cal.get(Calendar.HOUR_OF_DAY) + " : "
+                            + (this.cal.get(Calendar.MINUTE) < 10
+                            ? "0" + this.cal.get(Calendar.MINUTE)
+                            : this.cal.get(Calendar.MINUTE))
+                            + ":"
+                            + (this.cal.get(Calendar.SECOND) < 10
+                            ? "0" + this.cal.get(Calendar.SECOND)
+                            : this.cal.get(Calendar.SECOND));
+
+            // Notifier les observateurs
+            updateObservateur();
 
             try {
                 Thread.sleep(1000);
--- src/main/java/ca/qc/bdeb/a13/modele/Observable.java
+++ src/main/java/ca/qc/bdeb/a13/modele/Observable.java
@@ -3,5 +3,6 @@ package ca.qc.bdeb.a13.modele;
 public interface Observable {
     public void addObservateur(Observateur obs);
     public void updateObservateur();
-    public void delObservateur();
+    public void delObservateur(Observateur obs);
+    public void delAllObservateurs();
 }
